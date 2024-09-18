c4e764645d60cc23b7e713256dd1025f55b79aeb
seajumping <polula0716@gmail.com>
2024-09-18 16:29:32 +0800

    Update sync.js

--- api/sync.js
+++ api/sync.js
@@ -2,15 +2,14 @@
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-        // 假設您要處理 POST 請求
-        // 您可以在此處添加處理邏輯
+        // 處理 POST 請求
         const data = req.body;
-
-        // 返回成功響應
         res.status(200).json({ message: 'Data received', data });
+    } else if (req.method === 'GET') {
+        // 處理 GET 請求
+        res.status(200).json({ message: 'GET request received' });
     } else {
-        // 如果不是 POST 請求，返回 405 錯誤
+        // 如果不是支持的方法，返回 405 錯誤
         res.status(405).json({ error: 'Method not allowed' });
     }
 }
-
